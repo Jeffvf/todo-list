f981beab817ae82ff57d1410a6c92cbe36bf4389
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-09-28 23:11:09 -0300

    feat: show header and footer on page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { makeHeader } from "./header";
+import { makeHeader } from "./header.js";
+import css from './styles.css'
+import {sidebar} from './sidebar.js';
+import {footer} from './footer.js'
 
 makeHeader.addElements();
+
+sidebar.createSidebar();
+
+footer.insertFooterElements();
