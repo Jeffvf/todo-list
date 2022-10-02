0ba4866372bc953c1be49dff5bbb7f5d0a81e0aa
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-10-01 23:38:03 -0300

    refactor: forms style and click events

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { makeHeader } from "./header.js";
 import css from './styles.css'
 import {body} from './body.js'
 import {footer} from './footer.js'
+import {forms} from './modules/forms'
 
 makeHeader.addElements();
 
 body.appendElements();
+
+forms.addClickEvents();
 
 footer.insertFooterElements();
