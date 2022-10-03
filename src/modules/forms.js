638cc7023b1f3c5185383e3b3d144e81fe8f478a
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-10-03 00:09:45 -0300

    feat: implement forms fields and task insertion

--- src/modules/forms.js
+++ src/modules/forms.js
@@ -1,16 +1,38 @@
+import {body} from '/home/jefferson/top/todo-list/src/body.js'
+
 const forms = (() => {
     const modal = document.getElementById("myModal");
     const span = document.getElementsByClassName("close")[0];
+    const btn = document.getElementById("create-btn");
 
-    const addClickEvents = () => {
+    const addClickEvents = (proj) => {
         span.onclick = function() {
             modal.style.display = "none";
         }
-    
+        
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
+        }
+
+        btn.onclick = function(){
+            const title = document.getElementById('title').textContent;
+            const description = document.getElementById('description').value;
+            const date = document.getElementById('date').value;
+            const priorityList = document.getElementsByName('priority');
+            
+            let priority;
+            for(let i = 0; i < priorityList.length; i++){
+                if(priorityList[i].checked){
+                    priority = priorityList[i].value;
+                    break;
+                }
+            }
+            
+            proj.add(title, description, date, priority);
+            
+            body.appendElements(proj);
         }
     }
 
@@ -19,4 +41,3 @@ const forms = (() => {
 })();
 
 export {forms};
-
