6e8401db73206d413229576253f63bcb71d38d5e
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-10-09 16:19:00 -0300

    refactor: load all modules

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import css from './styles.css'
 import {body} from './body.js'
 import {footer} from './footer.js'
 import {forms} from './modules/forms.js'
-import {Projects, Project} from './modules/project.js'
+import {project} from './modules/project.js'
 
 makeHeader.addElements();
 
 if(!localStorage.getItem('projects')){
-    const projects = new Projects();
+    const projects = new project.Projects();
     
     const proj = projects.add('Inbox');
+    projects.add('Today');
+    projects.add('Upcoming');
 
     localStorage.setItem('projects', JSON.stringify(projects.projectList));
-}
-else{
-    const projects = new Projects();
-    const localProjects = localStorage.getItem('projects');
-
-    projects.projectList = localProjects;
+    localStorage.setItem('currentProject', JSON.stringify(proj));
 }
 
 const currentProject = JSON.parse(localStorage.getItem('currentProject') || "[]");
 
-const newProj = new Project(currentProject['name']);
+const newProj = new project.Project(currentProject['name']);
 
 newProj.taskList = currentProject.taskList;
 
-forms.addClickEvents(newProj);
+project.loadTodayTasks();
+project.loadUpcomingTasks();
+
+forms.addClickEvents();
 body.appendElements();
 
 footer.insertFooterElements();
